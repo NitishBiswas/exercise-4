5a702e3a0f0852967dc0a10fc1041205573541d8
Nitish Biswas <55139122+NitishBiswas@users.noreply.github.com>
2022-11-04 05:03:15 +0600

    Deploy exercise-4

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="details" element={<Details />} />
+          <Route path="*" element={<Home />} />
         </Routes>
       </div>
     </div>

d8e9c6d8cb9cb69414c1815d6bb10f45cb3183bf
Nitish Biswas <55139122+NitishBiswas@users.noreply.github.com>
2022-11-04 04:56:43 +0600

    Deploy exercise-4

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Home from './pages/home/Home';
+import { Routes, Route } from "react-router-dom";
+import Details from './pages/details/Details';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="details" element={<Details />} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default App;
